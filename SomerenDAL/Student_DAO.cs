361e7a5cd13caebfe64c01f74ab92afac67caa89
BramTerlouw <70586317+BramTerlouw@users.noreply.github.com>
2021-03-02 20:00:51 +0100

    test

---
+++ SomerenDAL/Student_DAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.ObjectModel;
+using SomerenModel;
+
+namespace SomerenDAL
+{
+    public class Student_DAO : Base
+    {
+      
+        public List<Student> Db_Get_All_Students()
+        {
+            string query = "SELECT id, name FROM student";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Student> ReadTables(DataTable dataTable)
+        {
+            List<Student> students = new List<Student>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Student student = new Student()
+                {
+                    Number = Convert.ToInt32(dr["id"]),/*(int)dr["id"],*/
+                    Name = (String)(dr["name"].ToString())
+                };
+                students.Add(student);
+            }
+            return students;
+        }
+
+    }
+}
