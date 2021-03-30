9ba33d0413d4ae21cdfdce56969429ba3af0d961
Merlijn Busch <merlijn1busch@gmail.com>
2021-03-30 12:49:35 +0200

    added poassword checks to make sure its a good password

--- SomerenUI/SomerenUI.cs
+++ SomerenUI/SomerenUI.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace SomerenUI
@@ -414,6 +415,9 @@ namespace SomerenUI
                 return;
             }
 
+            if (!ValidatePassword(password))
+                return;
+
             // add user
             register_Service.InsertUser(name, email, SHA512(password), question, answer);
 
@@ -512,6 +516,9 @@ namespace SomerenUI
                 return;
             }
 
+            if (!ValidatePassword(password))
+                return;
+
             register_Service.updateUserPassword(email, SHA512(password));
 
             MessageBox.Show("Updated password and succesfully logged in");
@@ -524,6 +531,54 @@ namespace SomerenUI
             menuStrip1.Show();
             pnl_Dashboard.Show();
 
+        }
+
+        private bool ValidatePassword(string password)
+        {
+            var input = password;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new Exception("Password should not be empty");
+            }
+
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasMiniMaxChars = new Regex(@".{8,15}");
+            var hasLowerChar = new Regex(@"[a-z]+");
+            var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
+
+
+            if (!hasLowerChar.IsMatch(input))
+            {
+                MessageBox.Show("Password should contain At least one lower case letter");
+                return false;
+            }
+            else if (!hasUpperChar.IsMatch(input))
+            {
+                MessageBox.Show("Password should contain At least one upper case letter");
+                return false;
+            }
+            else if (!hasMiniMaxChars.IsMatch(input))
+            {
+                MessageBox.Show("Password should not be less than or greater than 12 characters");
+                return false;
+            }
+            else if (!hasNumber.IsMatch(input))
+            {
+                MessageBox.Show("Password should contain At least one numeric value");
+                return false;
+            }
+
+            else if (!hasSymbols.IsMatch(input))
+            {
+                MessageBox.Show("Password should contain At least one special case characters");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
 
